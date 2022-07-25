6486c678ecccf31acecc2d3935fce691670e8cf5
Yudi Alviska <yudi.a@weefer.co.id>
2022-07-25 18:02:08 +0700

    test android

--- android/src/main/java/com/reactnativetflite/TfliteModule.java
+++ android/src/main/java/com/reactnativetflite/TfliteModule.java
@@ -1,5 +1,8 @@
 package com.reactnativetflite;
 
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Promise;
@@ -8,9 +11,17 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 @ReactModule(name = TfliteModule.NAME)
 public class TfliteModule extends ReactContextBaseJavaModule {
   public static final String NAME = "Tflite";
+  private Interpreter interpreter;
 
   public TfliteModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -22,17 +33,39 @@ public class TfliteModule extends ReactContextBaseJavaModule {
     return NAME;
   }
 
-
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  public void initTensor(String modelName, String modelLabel, int count, Promise promise) {
-    promise.resolve(count);
+  public void initTensor(String modelFile, int count, Promise promise) {
+    AssetManager assetManager = getReactApplicationContext().getAssets();
+    try {
+      Interpreter.Options options = new Interpreter.Options();
+      options.setNumThreads(count);
+      interpreter = new Interpreter(loadModelFile(assetManager, modelFile), options);
+      interpreter.allocateTensors();
+      promise.resolve("initialization tflite success");
+    } catch (Exception e) {
+      e.printStackTrace();
+      promise.reject(new Throwable(e));
+      return;
+    }
   }
 
   @ReactMethod
   public void tensorImage(String imagePath, Promise promise) {
     promise.resolve(imagePath);
+  }
+
+  private static MappedByteBuffer loadModelFile(AssetManager assets, String modelFilename)
+    throws IOException {
+    AssetFileDescriptor fileDescriptor = assets.openFd(modelFilename);
+    FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+    FileChannel fileChannel = inputStream.getChannel();
+    long startOffset = fileDescriptor.getStartOffset();
+    long declaredLength = fileDescriptor.getDeclaredLength();
+    return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+  }
+
+  class ObjTensor {
+
   }
 
 }
