82d21dc99a191c1fc0df34b6504d1858cc9047e9
Yudi Alviska <yudi.a@weefer.co.id>
2022-08-02 14:41:25 +0700

    fix: clean code

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,4 @@
-// import type { Frame } from 'react-native-vision-camera';
 import { NativeModules, Platform } from 'react-native';
-
-/**
- * Scans Faces.
- */
-
-// type Point = { x: number; y: number };
-// export interface Face {
-//   leftEyeOpenProbability: number;
-//   rollAngle: number;
-//   pitchAngle: number;
-//   yawAngle: number;
-//   rightEyeOpenProbability: number;
-//   smilingProbability: number;
-//   bounds: {
-//     y: number;
-//     x: number;
-//     height: number;
-//     width: number;
-//   };
-//   contours: {
-//     FACE: Point[];
-//     NOSE_BOTTOM: Point[];
-//     LOWER_LIP_TOP: Point[];
-//     RIGHT_EYEBROW_BOTTOM: Point[];
-//     LOWER_LIP_BOTTOM: Point[];
-//     NOSE_BRIDGE: Point[];
-//     RIGHT_CHEEK: Point[];
-//     RIGHT_EYEBROW_TOP: Point[];
-//     LEFT_EYEBROW_TOP: Point[];
-//     UPPER_LIP_BOTTOM: Point[];
-//     LEFT_EYEBROW_BOTTOM: Point[];
-//     UPPER_LIP_TOP: Point[];
-//     LEFT_EYE: Point[];
-//     RIGHT_EYE: Point[];
-//     LEFT_CHEEK: Point[];
-//   };
-// }
 
 export function tflite(frame: any): any {
   'worklet';
