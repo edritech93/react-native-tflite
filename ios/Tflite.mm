de0a61327dd947f7a214c28fa6f3369f1acec62e
Yudi Alviska <yudi.a@weefer.co.id>
2023-08-07 17:19:28 +0700

    feat: test edit new arch on iOS

--- ios/Tflite.mm
+++ ios/Tflite.mm
@@ -5,10 +5,22 @@ RCT_EXPORT_MODULE()
 
 // Don't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
-- (NSNumber *)multiply:(double)a b:(double)b {
-    NSNumber *result = @(a * b);
+//- (NSNumber *)multiply:(double)a b:(double)b {
+//    NSNumber *result = @(a * b);
+//
+//    return result;
+//}
 
-    return result;
+- (void)initTensor:(NSString *)modelPath count:(double)count resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    
+}
+
+- (void)tensorBase64:(NSString *)imageString resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    
+}
+
+- (void)tensorImage:(NSString *)imagePath resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
